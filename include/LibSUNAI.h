31ffa471c6ee8c396a5acdf8e69b6071b142d388
Xavier Baro <xavierbaro@gmail.com>
2012-03-15 09:46:07 +0100

    Added basic library structure

---
+++ include/LibSUNAI.h
@@ -0,0 +1,32 @@
+/*
+	Copyright 2011-2012 Xavier Baró
+
+	This file is part of LibSUNAI.
+
+    LibSUNAI is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    LibSUNAI is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#ifndef __LIBSUNAI__H
+#define __LIBSUNAI__H
+
+#include "compatibility.h"
+
+namespace LibSUNAI {
+
+	class LIBSUNAI_API Class1
+	{
+		// TODO: Add your methods for this class here.
+	};
+}
+
+#endif // __LIBSUNAI__H
---
+++ include/compatibility.h
@@ -0,0 +1,54 @@
+/*
+	Copyright 2011-2012 Xavier Baró
+
+	This file is part of LibSUNAI.
+
+    LibSUNAI is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    LibSUNAI is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#ifndef __COMPATIBILITY_H
+#define __COMPATIBILITY_H
+
+#ifdef _WIN32
+#include <windows.h>
+	//! Windows definitions
+	#ifdef LIBSUNAI_EXPORTS
+		#define LIBSUNAI_API __declspec(dllexport)
+	#else
+		#define LIBSUNAI_API __declspec(dllimport)
+	#endif
+	
+	#define isinf(x) !_finite(x)
+	#define isnan(x) _isnan(x)
+	
+	#include <math.h>
+	#define RAND_VALUE() (static_cast<double>(rand())/static_cast<double>(RAND_MAX))	
+#else
+	//! Linux definitions
+	#include <stdlib.h>
+	#define LIBSUNAI_API 
+	#define RAND_VALUE() ((double)random()/(double)RAND_MAX)
+#endif
+
+//! Additional definitions
+#ifndef PI
+	#define PI 3.14159265358979323846
+#endif
+#ifndef EPS
+	#define EPS 0.000001
+#endif
+#ifndef IO_CLASSNAME_LEN
+	#define IO_CLASSNAME_LEN 50
+#endif
+
+#endif //__COMPATIBILITY_H
